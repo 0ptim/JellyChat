ba544f2fb7c4b1de4b384a3b1f75a49da8d6ac65
Joshua Kummer <kummer.joshua@outlook.com>
2023-01-25 20:16:18 +0100

    Add `similarity_top_k` param

--- Python/Experiment2/app.py
+++ Python/Experiment2/app.py
@@ -7,5 +7,5 @@ index_from_disk = GPTSimpleVectorIndex.load_from_disk('./indices/index_wiki.json
 
 while True:
     question = input('Ask anything about DeFiChain: ')
-    response = index_from_disk.query(question, verbose=True)
+    response = index_from_disk.query(question, verbose=True, similarity_top_k=2)
     print(response)
