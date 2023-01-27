0839c729e8bf87c97b44958fe90ba98d22f8dba6
Joshua Kummer <kummer.joshua@outlook.com>
2023-01-27 13:06:21 +0100

    Make endpoint using gpt to predict the answer

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,28 @@
+from gpt_index import GPTSimpleVectorIndex
+from dotenv import load_dotenv
 from flask import Flask
 from flask import request
+
+load_dotenv()
+
+index_from_disk = GPTSimpleVectorIndex.load_from_disk(
+    './indices/index_wiki.json')
 
 app = Flask(__name__)
 
 
 @app.post("/get-response")
 def process_question():
+    print("▶️")
+
     if request.is_json and 'question' in request.json:
         question = request.json["question"]
         print("Question asked:", question)
+
+    response = index_from_disk.query(
+        question, verbose=True, similarity_top_k=1)
+    print(response)
+
     return {
-        "respone": "Answer"
+        "response": response.response
     }
