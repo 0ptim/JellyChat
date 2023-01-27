93f498a55f2b598edd80ecd85aeaf8f4b0011b51
Joshua Kummer <kummer.joshua@outlook.com>
2023-01-27 16:06:02 +0100

    Add JWT auth to Flask endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,37 @@
 from gpt_index import GPTSimpleVectorIndex
 from dotenv import load_dotenv
-from flask import Flask
+import os
+from flask import Flask, jsonify, request
 from flask import request
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+
 
 load_dotenv()
+
+app = Flask(__name__)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.environ["JWT_SECRET_KEY"]
+jwt = JWTManager(app)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != os.environ["JWT_USERNAME"] or password != os.environ["JWT_PASSWORD"]:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
 
 index_from_disk = GPTSimpleVectorIndex.load_from_disk(
     './indices/index_wiki.json')
 
-app = Flask(__name__)
-
 
 @app.post("/get-response")
+@jwt_required()
 def process_question():
     print("▶️")
 
