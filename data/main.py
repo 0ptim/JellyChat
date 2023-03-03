e9f990a86daa273e290fb4a92e295a13e72d57b2
Joshua Kummer <kummer.joshua@outlook.com>
2023-03-04 00:14:41 +0100

    Add script to convert json qa data to csv

---
+++ data/main.py
@@ -0,0 +1,19 @@
+import json
+import csv
+
+# open the JSON file with the correct encoding
+with open('qa.json', encoding='utf-8') as f:
+    data = json.load(f)
+
+# remove newlines from the "answer" strings
+for entry in data:
+    entry['answer'] = entry['answer'].replace('\n', ' ')
+
+# write the filtered data to a CSV file
+with open('qa.csv', mode='w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    # write header row
+    writer.writerow(['id', 'question', 'answer', 'rating', 'date'])
+    for entry in data:
+        writer.writerow([entry['id'], entry['question'],
+                        entry['answer'], entry['rating'], entry['date']])
