bf904c3735f63708c69cd9ae865c1442a63629a0
Joshua Kummer <kummer.joshua@outlook.com>
2023-03-20 22:07:59 +0100

    Implement vector source with Qdrant

--- job/wiki_scraper.py
+++ job/wiki_scraper.py
@@ -1,17 +1,17 @@
-from llama_index import SimpleWebPageReader, GPTSimpleVectorIndex
-from dotenv import load_dotenv
+from langchain.vectorstores import Qdrant
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import CharacterTextSplitter
+from dotenv import load_dotenv, get_key
 from sitemap_parser import *
 import re
-import logging
-import sys
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 load_dotenv()
 
 scrapeUrl = 'https://www.defichainwiki.com/'
 indexFileTarget = './indices/index_wiki.json'
+chunk_size = 2000
+chunk_overlap = 200
 
 urls = getUrlsToScrape(scrapeUrl)
 
@@ -20,18 +20,33 @@ for url in urls:
         print("Remove", url)
         urls.remove(url)
 
+
 print('🔭 Scrape %s found pages..' % len(urls))
-documents = SimpleWebPageReader(html_to_text=True).load_data(urls)
-print('✅ Scraped %s pages' % len(documents))
+loader = UnstructuredURLLoader(urls=urls)
+docs = loader.load()
+print('✅ Scraped %s pages' % len(docs))
 
 
-print('Remove long strings')
-for document in documents:
-    document.text = re.sub(
-        r'(?<=\S)[^\s]{' + str(3714) + ',}(?=\S)', '', document.text)
+print('➖ Remove long strings..')
+for document in docs:
+    document.page_content = re.sub(
+        r'(?<=\S)[^\s]{' + str(chunk_size) + ',}(?=\S)', '', document.page_content)
+print('✅ Removed long strings')
 
-print('Get simple vector index via GPT API..')
-index = GPTSimpleVectorIndex(documents)
 
-print('💾 Saving index to:', indexFileTarget)
-index.save_to_disk(indexFileTarget)
+print('🗨 Split into chunks..')
+text_splitter = CharacterTextSplitter(
+    chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+docs = text_splitter.split_documents(docs)
+print('✅ Split into %s chunks' % len(docs))
+
+print('🔮 Embedding..')
+embeddings = OpenAIEmbeddings()
+
+qdrant = Qdrant.from_documents(
+    docs, embeddings,
+    url=get_key('.env', 'QDRANT_HOST'),
+    api_key=get_key('.env', 'QDRANT_API_KEY'),
+    prefer_grpc=True,
+    collection_name='DeFiChainWiki')
+print('✅ Embedded')
