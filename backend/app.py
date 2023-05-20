e622dd8935596f19c55aaee87824c355327b3bf8
Joshua Kummer <kummer.joshua@outlook.com>
2023-05-20 22:13:17 +0200

    Get tool message for tool name function

--- backend/app.py
+++ backend/app.py
@@ -36,9 +36,32 @@ class CustomHandler(BaseCallbackHandler):
     def on_tool_start(self, serialized: Dict[str, Any], input_str: str, **kwargs: Any) -> Any:
         """ Emit the action name to the client, so the user can see what jellychat is doing."""
         print(f"🔥 Tool started: {serialized}")
-        emit("tool_start", {"tool_name": serialized["name"]})
+        tool_message = get_tool_message(serialized["name"])
+        emit("tool_start", {"tool_name": tool_message})
         # Yield control back to the caller, so the client can receive the event instantly.
         socketio.sleep(0)
+
+
+def get_tool_message(name: str) -> str:
+    """depending on the tool name, return a message to the user"""
+    if name == "DeFiChainWiki QA System":
+        return "I'll go look this up in the DeFiChainWiki for you 🔎"
+    elif name == "Get Stats":
+        return "Let me gather the latest blockchain statistics for you 📊"
+    elif name == "Get Token Balance":
+        return "Checking the token balance now ⚖️"
+    elif name == "Get Transactions":
+        return "Fetching the transaction history 🔄"
+    elif name == "Get UTXO Balance":
+        return "Let's check the UTXO balance 💱"
+    elif name == "Get Vaults for Address":
+        return "Analyzing vaults associated with the address 🏦"
+    elif name == "Get Vault Information":
+        return "Retrieving detailed vault information ℹ️"
+    elif name == "Calculator":
+        return "Let's do the math together 🧮"
+    else:
+        raise ValueError(f"Unknown tool name: {name}")
 
 
 def log_response_info(response_obj, callback_obj):
