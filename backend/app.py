3f56879dc518b473c720fe93b5af5511dfe72e6f
Joshua Kummer <kummer.joshua@outlook.com>
2023-05-20 18:52:07 +0200

    Add back REST endpoint

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from langchain.callbacks import get_openai_callback
 from langchain.callbacks.base import BaseCallbackHandler
 from session_agents import agent_for_user
 from data import SupabaseManager
+from functools import wraps
 
 
 # Setup
@@ -17,33 +18,25 @@ socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
 manager = SupabaseManager()
 
 
+def cors_headers(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        response = f(*args, **kwargs)
+        response.headers.set('Access-Control-Allow-Origin', '*')
+        response.headers.set('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        response.headers.set('Access-Control-Allow-Headers',
+                             'API-Key, Content-Type')
+        response.headers.set('Access-Control-Max-Age', '3600')
+        return response
+
+    return decorated_function
+
+
 class CustomHandler(BaseCallbackHandler):
     def on_tool_start(self, serialized: Dict[str, Any], input_str: str, **kwargs: Any) -> Any:
         """ Emit the action name to the client, so the user can see what jellychat is doing."""
+        print(f"🔥 Tool started: {serialized}")
         emit("tool_start", {"tool_name": serialized["name"]})
-
-
-@socketio.on("user_input")
-def process_question(question, user_token):
-    if not user_token:
-        emit("error", {"error": "User token is required"})
-        return
-
-    if not question:
-        emit("error", {"error": "Question is required"})
-        return
-
-    jelly_chat_agent = agent_for_user(user_token)
-
-    with get_openai_callback() as cb:
-        response_obj = jelly_chat_agent(
-            question, callbacks=[CustomHandler()])
-        log_response_info(response_obj, cb)
-
-    response = response_obj["output"].strip()
-    manager.add_qa(question, response)
-
-    emit("final_message", {"message": response})
 
 
 def log_response_info(response_obj, callback_obj):
@@ -54,14 +47,70 @@ def log_response_info(response_obj, callback_obj):
     print(f"Total Cost (USD): ${callback_obj.total_cost}")
 
 
-@app.route('/qa', methods=["OPTIONS", "GET"])
-def get_all_qa():
+def extract_data(json_request, field):
+    return json_request.get(field, "").strip()
+
+
+@socketio.on("user_message")
+def process_input(user_token, message):
+    if not user_token:
+        emit("error", {"error": "User token is required"})
+        return
+
+    if not message:
+        emit("error", {"error": "Input is required"})
+        return
+
+    jelly_chat_agent = agent_for_user(user_token)
+
+    with get_openai_callback() as cb:
+        response_obj = jelly_chat_agent(
+            message, callbacks=[CustomHandler()])
+        log_response_info(response_obj, cb)
+
+    response = response_obj["output"].strip()
+    manager.add_message_response(message, response)
+
+    emit("final_message", {"message": response})
+
+
+@app.route("/user_message", methods=["OPTIONS", "POST"])
+@cors_headers
+def process_input():
     if request.method == "OPTIONS":
         return make_response("", 204)
 
-    QA = manager.get_qa()
+    if not request.is_json:
+        return make_response("Request should be in JSON format", 400)
 
-    return make_response(jsonify(QA), 200)
+    user_token = extract_data(request.json, "user_token")
+    if not user_token:
+        return jsonify({"error": "User token is required"}), 400
+
+    message = extract_data(request.json, "message")
+    if not message:
+        return jsonify({"error": "Message is required"}), 400
+
+    jelly_chat_agent = agent_for_user(user_token)
+
+    with get_openai_callback() as cb:
+        response_obj = jelly_chat_agent(message)
+        log_response_info(response_obj, cb)
+
+    response = response_obj["output"].strip()
+    manager.add_message_response(message, response)
+
+    return make_response(jsonify({"response": response}), 200)
+
+
+@app.route('/messages_answers', methods=["OPTIONS", "GET"])
+def get_all_messages_answers():
+    if request.method == "OPTIONS":
+        return make_response("", 204)
+
+    messages_answers = manager.get_messages_answers()
+
+    return make_response(jsonify(messages_answers), 200)
 
 
 if __name__ == "__main__":
