2ba6b89eea2a33f74d6f3f18c34262c65035a87c
eric-volz <eric@volz.link>
2023-06-22 22:44:47 +0200

    adjustment for vNext

--- backend/utils.py
+++ backend/utils.py
@@ -1,28 +1,13 @@
 from tools.wiki_qa import wikiTool
-from tools.ocean.stats import statsTool
-from tools.ocean.token_balance import tokenbalanceTool
-from tools.ocean.transactions import transactionsTool
-from tools.ocean.utxo_balance import utxoTool
-from tools.ocean.vaults import vaultsForAddressTool
-from tools.ocean.vault import vaultInformationTool
+from tools.ocean import oceanTools
 
 
 def get_tool_message(name: str) -> str:
     """depending on the tool name, return a message to the user"""
     if name == wikiTool.name:
         return "I'll go look this up in the DeFiChainWiki for you 🔎"
-    elif name == statsTool.name:
-        return "Let me gather the latest blockchain statistics for you 📊"
-    elif name == tokenbalanceTool.name:
-        return "Checking the token balance now ⚖️"
-    elif name == transactionsTool.name:
-        return "Fetching the transaction history 🔄"
-    elif name == utxoTool.name:
-        return "Let's check the UTXO balance 💱"
-    elif name == vaultsForAddressTool.name:
-        return "Analyzing vaults associated with the address 🏦"
-    elif name == vaultInformationTool.name:
-        return "Retrieving detailed vault information ℹ️"
+    elif name in [tool.name for tool in oceanTools]:
+        return "Let me gather some information out of the Ocean for you 🌊"
     elif name == "Calculator":
         return "Let's do the math together 🧮"
     else:
