545beb268d32d8c05f185d2bc136d0700c77771a
eric-volz <eric@volz.link>
2023-07-31 11:34:48 +0200

    implement answer on replied message: discord

--- bots/discord/discord_bot.py
+++ bots/discord/discord_bot.py
@@ -50,17 +50,23 @@ class JellyChatDiscordBot(discord.Client):
         The Bot only answers if he is tagged
         """
         # Only generates and sends message if bot is tagged
+        reply = None
+        if message.reference:
+            reply = await self.fetch_message(message, message.reference.message_id)
+
         if self.user in message.mentions:
-            await self.send(message, self.get_answer(message, False), False)
+            answer: str = self.get_answer(message, is_private=False, reply=reply)
+            await self.send(message, answer, is_private=False)
 
     async def private(self, message):
         """
         This method handles all messages of a private chat:
         It will always answer on anything that has been written into the chat
         """
-        await self.send(message, self.get_answer(message, True), True)
+        answer: str = self.get_answer(message, is_private=True)
+        await self.send(message, answer, is_private=True)
 
-    def get_answer(self, message, is_private: bool):
+    def get_answer(self, message, is_private: bool, reply=None):
         """
         This method queries an answer for the asked question
         """
@@ -72,8 +78,12 @@ class JellyChatDiscordBot(discord.Client):
         # Prompt that is appended to the question.
         extra_prompt = " The answer must be less than 2000 characters in length."
 
-        question = message.content.replace(f"<@{self.user.id}>", "")  #  + extra_prompt
-        print(f"New question from {message.author.name}: {question}")
+        message_content: str = message.content.replace(f"<@{self.user.id}>", "")  # + extra_prompt
+        question: str = message_content
+        if reply:
+            reply_content: str = reply.content.replace(f"<@{self.user.id}>", "")
+            question: str = f"{reply_content}\n\n{message_content}"
+
         return self.jellyChatAPI.user_message(userToken, question, JellyChatDiscordBot.APPLICATION)
 
     async def send(self, message, answer, is_private: bool):
@@ -82,11 +92,29 @@ class JellyChatDiscordBot(discord.Client):
         """
         print(f"Answer: {answer}")
         try:
-            await message.author.send(answer) if is_private else await message.channel.send(answer)
+            if is_private:
+                await message.author.send(answer, reference=message)
+            else:
+                await message.channel.send(answer, reference=message)
         except Exception as e:  # An error will occur if the answer is longer than 2000 characters
             print(e)
             msg = "Woooops, something went wrong while trying to ship the answer to you."
-            await message.author.send(msg) if is_private else await message.channel.send(msg)
+            if is_private:
+                await message.author.send(msg, reference=message)
+            else:
+                await message.channel.send(msg, reference=message)
+
+    async def fetch_message(self, ctx, message_id: int):
+        try:
+            # Fetch the message from the channel using the message_id
+            message = await ctx.channel.fetch_message(message_id)
+            return message
+        except discord.NotFound:
+            print("Message not found.")
+        except discord.Forbidden:
+            print("Bot doesn't have permission to access the message.")
+        except discord.HTTPException:
+            print("An error occurred during the HTTP request.")
 
 
 if __name__ == '__main__':
