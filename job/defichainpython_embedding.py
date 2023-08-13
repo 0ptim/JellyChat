c1753462758577e1cc3dc7382986078ba62d69a9
eric-volz <eric@volz.link>
2023-08-13 21:10:46 +0200

    complete rework of embeddings creation for DefichainPython

--- job/defichainpython_embedding.py
+++ job/defichainpython_embedding.py
@@ -2,7 +2,6 @@ import os
 import re
 from langchain.vectorstores import SupabaseVectorStore
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from supabase.client import Client, create_client
 from dotenv import load_dotenv
 import uuid
@@ -14,8 +13,6 @@ load_dotenv()
 
 vectorTableName = "embeddings"
 scrapeUrls = ["https://docs.defichain-python.de/build/html/sitemap.xml"]
-chunk_size = 1000
-chunk_overlap = 50
 embedding_model = "text-embedding-ada-002"
 
 supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
@@ -30,7 +27,6 @@ print("🔎 Found %s pages in total" % len(urls))
 # Remove duplicates
 urls = list(dict.fromkeys(urls))
 print("🔎 Found %s unique pages" % len(urls))
-
 
 # Remove urls
 remove_urls = (
@@ -54,33 +50,6 @@ for doc in docs:
     print("📄 Content:", doc.page_content.replace("\n", " ")[:100] + "...")
     print("---")
 
-print("➖ Remove long strings")
-for document in docs:
-    document.page_content = re.sub(
-        r"(?<=\S)[^\s]{" + str(chunk_size) + ",}(?=\S)", "", document.page_content
-    )
-print("✅ Removed long strings")
-
-
-print("🗨 Split into chunks..")
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=chunk_size,
-    chunk_overlap=chunk_overlap,
-    length_function=len,
-    separators=["\n\n", "\n", " ", ""],
-)
-docs = text_splitter.split_documents(docs)
-print("✅ Split into %s chunks" % len(docs))
-
-# import tiktoken
-
-# enc = tiktoken.get_encoding("cl100k_base")
-# for doc in docs:
-#     print("🔖 Title:", doc.metadata["title"])
-#     print("📄 Content:", doc.page_content.replace("\n", " ")[:100] + "...")
-#     tokens = enc.encode(doc.page_content)
-#     print("⚡ Tokens:", len(tokens))
-
 print("➖ Remove all old documents from table")
 supabase.table(vectorTableName).delete().neq("id", uuid.uuid1()).execute()
 print("✅ Removed all old documents from table")
@@ -91,7 +60,7 @@ upload_chunk_size = 200
 
 # Split the documents in chunks for upload (Did time out when too large).
 docs_chunks = [
-    docs[x : x + upload_chunk_size] for x in range(0, len(docs), upload_chunk_size)
+    docs[x: x + upload_chunk_size] for x in range(0, len(docs), upload_chunk_size)
 ]
 
 # Iterate over each chunk and upload separately.
